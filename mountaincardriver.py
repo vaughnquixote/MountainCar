a042009498a2813957b109fc7b39e2918870f830
Vaughn Franz <vaughnfranz@gmail.com>
2020-11-21 18:23:00 -0500

    starting to set up agent class and implementing algorithm, not working yet

--- mountaincardriver.py
+++ mountaincardriver.py
@@ -1,14 +1,26 @@
 import gym 
 
-env = gym.make('MountainCar-v0')
+GAMMA = 0.85
+ALPHA = 0.4
+
+def main():
+
+	q_values = dict()
+
+	env = gym.make('MountainCar-v0')
+
+	print(env.action_space)
+
+	env.reset()
+
+	for _ in range(10):
+
+		env.render()
+		action = env.action_space.sample()
+		# actions are 0, 1 or 2 corresponding to push left, no push and push right
+		observation, reward, done, info = env.step(action)
+		print(observation)
 
 
-env.reset()
+	env.close()
 
-for _ in range(1000):
-
-	env.render()
-	env.step(env.action_space.sample())
-
-	
-env.close()

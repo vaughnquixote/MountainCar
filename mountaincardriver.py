7924e82f28516ff6e33c976d536f5f22fa784377
Vaughn Franz <vaughnfranz@gmail.com>
2020-12-07 21:44:02 -0500

    adjusted winning case, refactored main agent class, setting up infrastructure for recording results and analysis

--- mountaincardriver.py
+++ mountaincardriver.py
@@ -2,6 +2,7 @@ import gym
 import numpy as np 
 import random
 import time
+import matplotlib.pyplot as plt
 
 GAMMA = 0.85
 ALPHA = 0.4
@@ -17,8 +18,8 @@ class MountainCarAgent():
 		self.env = gym.make('MountainCar-v0')
 		self.episodes = episodes
 		self.rewards = []
-		print(self.env.observation_space.low)
-		print(self.env.observation_space.high)
+		self.winning_episodes = []
+		self.final_positions = []
 
 
 	def getQValue(self, position, velocity, action):
@@ -37,13 +38,12 @@ class MountainCarAgent():
 		prev_velocity = round(state[1], 2)
 		total_reward = 0
 		done = False
-		random_action_counter = 0
-		non_random_counter = 0
 
 		while done != True:
 
-			if num_episode > self.episodes - 5:
-				self.env.render()
+			# if num_episode > self.episodes - 5 or num_episode < 5:
+			# 	self.env.render()
+
 
 			if random.random() > self.epsilon:
 				action = None
@@ -54,15 +54,19 @@ class MountainCarAgent():
 					if samp_q > max_q:
 						action = a
 						max_q = samp_q
-				non_random_counter += 1
 			else:			
 				action = self.env.action_space.sample()
-				random_action_counter += 1
 
 			observation, reward, done, info = self.env.step(action)
+
 			position = round(observation[0], 1)
 			velocity = round(observation[1], 2)
-			
+
+			if done and position >= 0.5:
+				self.q_values[(prev_position, prev_velocity, action)] = reward
+				total_reward += reward
+				break
+
 			current_q = self.q_values[(prev_position, prev_velocity, action)]
 			
 			qs = []
@@ -79,17 +83,48 @@ class MountainCarAgent():
 			prev_velocity = velocity 
 			total_reward += reward
 
-		return total_reward, random_action_counter, non_random_counter
+		if done == True and position >= 0.5:
+
+			self.wins.append(num_episode)
+
+		self.final_positions.append(position)
+
+		return total_reward
 
 	def train(self):
 
-		randoms = 0
-
-		non_randoms = 0
-
-		done_pos = False
+		episodes = []
 
 		epsilon_change = self.epsilon/self.episodes
+
+		for _ in range(self.episodes):	
+
+			reward= self.run_episode(_)
+
+			self.rewards.append(reward)
+
+			episodes.append(_)
+
+			if self.epsilon > 0.001:
+
+				self.epsilon -= epsilon_change
+
+			# if(_ < 5 or _ > self.episodes - 5):
+			# 	time.sleep(10)
+
+
+		# plt.plot(episodes, self.rewards)
+		# plt.title("Test plot 1")
+		# plt.xlabel("Episode")
+		# plt.ylabel("Reward")
+		# plt.show()
+
+
+		self.env.close()
+
+	def initialize_q_values(self):
+
+		done_pos = False
 
 		pos_count = -1.2
 
@@ -121,44 +156,62 @@ class MountainCarAgent():
 
 				done_pos = True
 
-
-		for _ in range(self.episodes):	
-
-			reward, rand, non_rand = self.run_episode(_)
-
-			randoms += rand
-
-			non_randoms += non_rand
-
-			self.rewards.append(reward)
-
-			if self.epsilon > 0.001:
-
-				self.epsilon -= epsilon_change
-
-			# if (_ % 100) == 0:
-
-			# 	print("Episode: ", _)
-			# 	print("Average of last 100 rewards: ", sum(self.rewards[-100:-1])/100)
-
-
-		# for key, value in self.q_values.items():
-		# 	print("q value: ", key, value)
-
-		self.env.close()
-
 	def get_rewards(self):
 
 		return self.rewards
 
-def main():
-	start = time.time()
-	mc_test = MountainCarAgent(0.2, 0.9, 0.8, 5000)
+	def set_epsilon(self, new_ep):
 
+		self.epsilon = new_ep
+
+	def set_num_episodes(self, num_episodes):
+
+		self.num_episodes = num_episodes
+
+	def get_winning_episodes(self):
+
+		return self.winning_episodes
+
+
+def main():
+	mc_test = MountainCarAgent(0.2, 0.9, 0.8, 10000)
+	mc_test.initialize_q_values()
 	mc_test.train()
-	end = time.time()
+
+	wins = mc_test.get_winning_episodes()
+
+	alpha_variation = False
+	epsilon_variation = False
+	alpha = 0.05
+	epsilon = 0.05
+	while not alpha_variation: 
+		# alpha, gamma, epsilon, episodes
+		while not epsilon_variation:
+			mc = MountainCarAgent(alpha, 0.9, epsilon, 10000)
+			mc.initialize_q_values()
+			mc.train()
+
+			wins = mc.get_winning_episodes()
+			first_win = wins[0]
+
+			
+
+
+
+
+
+
+
+
+	# mc_test.set_epsilon(0)
+	# mc_test.set_num_episodes(100)
+	# mc_test.train()
+
+
+
 	print("time taken: ", end - start)
 	print("Maximum reward achieved: ", max(mc_test.get_rewards()))
 	print("Last ten rewards achieved: ", mc_test.get_rewards()[-10:-1])
+	print("Average reward achieved: ", sum(mc_test.get_rewards())/len(mc_test.get_rewards()))
 
 main()
